6d946fbda64364d2af0b7b6030758c76efab78d9
Aaron Watters <awatters@simonsfoundation.org>
2020-06-10 18:08:12 -0400

    don't use the main method

--- bytedesign.py
+++ bytedesign.py
@@ -146,6 +146,7 @@ class Designer(Turtle):
         self.centerpiece(s - (1.2 * scale), a, scale)
 
 def main():
+    "this bufferss too many messages"
     t = Designer()
     t.speed(0)
     t.hideturtle()
