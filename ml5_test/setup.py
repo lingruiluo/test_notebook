6a8c5d4d31a24f669100e5ff7802156c2c45dab0
lingruiluo <ll3356@columbia.edu>
2020-09-22 11:22:31 -0400

    test

---
+++ ml5_test/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+url = ""
+version = "0.1.0"
+readme = open('README.md').read()
+
+setup(
+    name="ml5_test",
+    packages=["ml5_test"],
+    version=version,
+    description="test version of ml5_ipynb",
+    long_description=readme,
+    include_package_data=True,
+    author="Lingrui Luo",
+    author_email="ll3356@columbia.edu",
+    url=url,
+    install_requires=[
+        "jp_proxy_widget", 
+        "jp_doodle",
+        "datetime", 
+        "numpy", 
+        "pandas",
+        "pandas_datareader", 
+        "pillow", 
+        "imageio",
+        "jupyter-ui-poll",
+        ],
+    license="MIT"
+)
